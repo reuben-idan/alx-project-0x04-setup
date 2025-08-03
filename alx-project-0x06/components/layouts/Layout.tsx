3424ec44f784745b694ee05647b91ae5a53f1619
reuben-idan <nobeadonis2@gmail.com>
2025-08-03 16:31:12 +0000

    feat: implement Redux for global state management
    - Add Redux store with counter slice
    - Update CounterApp to use Redux state and actions
    - Modify Header to display counter from Redux
    - Set up Redux Provider in _app.tsx

---
+++ alx-project-0x06/components/layouts/Layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import Header from '../layout/Header';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
